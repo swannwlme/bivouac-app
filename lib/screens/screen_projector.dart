6b8cf525c01f142e71973488be7c7b4af8f3c3fe
Swann Williame <swann.williame@gmail.com>
2024-07-11 15:29:00 +0200

    persistent nav bar plugin

--- lib/screens/screen_projector.dart
+++ lib/screens/screen_projector.dart
@@ -4,7 +4,9 @@ import 'package:bivouac/screens/main_screens/explore_screen.dart';
 import 'package:bivouac/screens/main_screens/home_screen.dart';
 import 'package:bivouac/screens/main_screens/profile_screen.dart';
 import 'package:bivouac/theme/color_palet.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:persistent_bottom_nav_bar/persistent_bottom_nav_bar.dart';
 
 class ScreenProjector extends StatefulWidget {
   const ScreenProjector({super.key});
@@ -13,53 +15,46 @@ class ScreenProjector extends StatefulWidget {
   State<ScreenProjector> createState() => _ScreenProjectorState();
 }
 
-class _ScreenProjectorState extends State<ScreenProjector> {
-  int selectedScreen = 0;
+class _ScreenProjectorState extends State<ScreenProjector> with TickerProviderStateMixin {
+  PersistentTabController controller = PersistentTabController(initialIndex: 0);
 
-  List screens = [
+
+  List<Widget> screens = [
     const HomeScreen(),
     const ExploreScreen(),
     const ClanScreen(),
     const ProfileScreen()
   ];
 
+  PersistentBottomNavBarItem navItem(IconData icon, String title){
+    return PersistentBottomNavBarItem(
+      icon: Icon(icon, size: 30,),
+      inactiveIcon: Icon(icon, size: 25,),
+      iconAnimationController: AnimationController(vsync: this, duration: const Duration(milliseconds: 400))..forward(),
+      title: title,
+      activeColorPrimary: Colpal.brown,
+      inactiveColorPrimary: CupertinoColors.systemGrey,
+    );
+  }
+
+  List<PersistentBottomNavBarItem> _navBarsItems() {
+        return [
+            navItem(Icons.home, "Home"),
+            navItem(Icons.search, "Search"),
+            navItem(Icons.group, "Friends"),
+            navItem(Icons.person, "Profile"),
+        ];
+    }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      bottomNavigationBar: BottomNavigationBar(
-        iconSize: 30,
-        type: BottomNavigationBarType.fixed,
-        selectedItemColor: Colpal.brown,
-        showUnselectedLabels: false,
-        selectedLabelStyle: const TextStyle(
-          fontWeight: FontWeight.w600
-        ),
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: 'Home',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.search),
-            label: 'Explore',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.group),
-            label: 'Clan',
-          ),
-          BottomNavigationBarItem(
-            icon: ProfileImage(size: 13,),
-            label: 'Profile',
-          )
-        ],
-        currentIndex: selectedScreen,
-        onTap: (index) {
-          setState(() {
-            selectedScreen = index;
-          });
-        },
-      ),
-      body: selectedScreen>=0 && selectedScreen<screens.length ? screens[selectedScreen] : screens[0],
-    );
+    return PersistentTabView(
+      context, 
+      screens: screens,
+      items: _navBarsItems(),
+      hideNavigationBarWhenKeyboardAppears: true,
+      navBarHeight: 60,
+      navBarStyle: NavBarStyle.style4
+      );
   }
 }
